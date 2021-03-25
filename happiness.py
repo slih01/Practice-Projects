53fbd95497618dc84046c8ce66cdcd0a3e6e53b3
Darren Haz <darrenhazan@gmail.com>
2021-03-25 09:34:38 +0000

    Adding integer only functionality to all inputs

--- happiness.py
+++ happiness.py
@@ -5,8 +5,9 @@
 # Requests from user number of integers for set n and m.
 while True:
     try:
-        n, m = map(int, input("Enter number of integers in set N, and number of integers "
-                              "in sets A & B separated by a space: ").split())
+        n, m = map(int, input("Enter the number of integers you want in set N, "
+                              "and number of integers in sets A & B separated by "
+                              "a space: ").split())
     except ValueError:
         print("Sorry you have not entered two integers separated by a space")
     else:
@@ -14,29 +15,41 @@ while True:
 
 # Ask for integer input for set N and checks it is correct length
 while True:
-    set_n = list(map(int, input("Enter {} integers for set N: ".format(n)).split()))
-    if len(set_n) != n:
-        print("Sorry you need to enter {} integers".format(n))
+    try:
+        set_n = list(map(int, input("Enter {} integers for set N: ".format(n)).split()))
+    except ValueError:
+        print("Sorry you have not entered integers")
     else:
-        break
+        if len(set_n) != n:
+            print("Sorry you need to enter {} integers".format(n))
+        else:
+            break
 
 # Ask for integer input for set A and checks it is correct length
 while True:
-    set_a = list(map(int, set(input("Enter {} different integers for set A: "
-                               .format(m)).split())))
-    if len(set_a) != m:
-        print("Sorry you need to enter {} unique integers".format(m))
+    try:
+        set_a = list(map(int, set(input("Enter {} unique integers for set A: "
+                                        .format(m)).split())))
+    except ValueError:
+        print("Sorry you have not entered integers")
     else:
-        break
+        if len(set_a) != m:
+            print("Sorry you need to enter {} unique integers".format(m))
+        else:
+            break
 
 # Ask for integer input for set B and checks it is correct length
 while True:
-    set_b = list(map(int,set(input("Now enter {} different integers for set B: "
-                  .format(m)).split())))
-    if len(set_b) != m:
-        print("Sorry you need to enter {} unique integers".format(m))
+    try:
+        set_b = list(map(int, set(input("Enter {} unique integers for set B: "
+                                        .format(m)).split())))
+    except ValueError:
+        print("Sorry you have not entered integers")
     else:
-        break
+        if len(set_b) != m:
+            print("Sorry you need to enter {} unique integers".format(m))
+        else:
+            break
 
 # Checks if and member of A is in N and adds one to happiness if so, and takes
 # off one if B is in N
